15e4ccb84006201482fce962c8db80a3d54ea164
TemoJokhadze <jokh1jokhadze02@gmail.com>
2025-03-27 18:07:11 +0400

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,36 @@ function App() {
           <img src={Group} alt="group" className="group" />
         </header>
         <main>
+          <div className="devizi">
+            <span className="spans1">EAT</span>
+            <span className="spans2">SLEEP</span>
+            <span className="spans3">SKINCARE</span>
+            <span className="spans3">REPEAT</span>
+          </div>
           <div className="cont1">
             <div className="spanss">
               <span className="span1">SKINCARE</span>
               <span className="span2">SPECIALIST</span>
             </div>
             <img src={Img1} alt="img1" className="img1" />
+          </div>
+          <div className="description">
+            <span className="desc1">
+              Iamze Gogidze – Certified Skincare Specialist
+            </span>
+            <span className="desc2">
+              With extensive experience and multiple certifications in skincare,
+              I am dedicated to helping clients achieve healthy, glowing skin.
+            </span>
+            <span>My approach combines expert knowledge with high-quality treatments
+            tailored to each individual’s needs.</span>
+            <span>Whether you’re looking for
+              rejuvenation, hydration, or solutions for specific skin concerns,
+              I provide personalized care to enhance your natural beauty.</span>
+            <span className="desc3">
+              Book a consultation today and discover the best version of your
+              skin!
+            </span>
           </div>
         </main>
       </div>
